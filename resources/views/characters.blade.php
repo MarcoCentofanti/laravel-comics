f19ff83e6c7f596038c860bcf92e0f6d8d9fab66
marcocentofanti <marcocentofanti@hotmail.it>
2026-09-15 17:32:19 +0200

    Bonus 1

---
+++ resources/views/characters.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title')
+Characters
+@endsection
+
+
+@section('content')
+<h1>I nostri SUPER Personaggi</h1>
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 
 Route::get('/', function () {
     return view('home');
+});
+
+Route::get('/characters', function(){
+    return View('characters');
 });
